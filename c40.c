aa376e9f8bda20e9c42ffa5d214c9794d70a1aad
Akhil Sodvadiya <113697861+AkhilS18@users.noreply.github.com>
2022-09-22 22:14:46 -0700

    Update c40.c

--- c40.c
+++ c40.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 main()
 {
+     // total digit in number :
+
      int a;
-     a=3/10;
-     printf("%d",a);
+     printf("enter a value : ");
+     scanf("%d",&a);
+
+     int i=1;
+     while (a>10)
+     {
+          a = a/10;
+          i++;
+     }
+     printf("number of digit : %d",i);
+     
 }
