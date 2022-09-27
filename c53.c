6f946f9469d5f2f7d68fe5a22e33ac94ce3c3409
Akhil Sodvadiya <113697861+AkhilS18@users.noreply.github.com>
2022-09-26 22:46:28 -0700

    Add files via upload

---
+++ c53.c
@@ -0,0 +1,19 @@
+//patern
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+#include<stdio.h>
+main()
+{
+    for(int i=1; i<=5; i++)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+}
