87f2a24993238cc0ffc5571602a8739f14401b41
Akhil Sodvadiya <113697861+AkhilS18@users.noreply.github.com>
2022-10-05 22:02:26 -0700

    Update c118.c

--- c118.c
+++ c118.c
@@ -2,40 +2,58 @@
 #include<stdio.h>
 main()
 {
-    
-                                //     under construction
-    
-  
-    
-    
     // check string is palingdrom or not.
 
-    char a[100];
+    char a[100],b[100];
     
     printf("enter string A : ");
     gets(a);
 
-    char b[100]=a;
     
-    int i,j=0,len=0;
+    
+    int i,j=0,len=0,count=0;
 
     for ( i = 0; a[i] != '\0'; i++)
     {
         len ++;
     }
-    //printf("%d",len);
 
-    for ( i = len; i >=0; i--)
+
+    for ( i = len-1; i >=0; i--)
     {
-        
-        printf("%c",a[i]);
-
+        b[j]=a[i];
+        j++;
     }
 
-    printf("\n%c",a[0]);
-    printf("\n%c",b[0]);
+
+    // for ( i = 0; i < len; i++)
+    // {
+    //     printf("%c",b[i]);
+        
+    // }
+
    
+    for ( i = 0; i < len; i++)
+    {
+        if (a[i]!=b[i])
+        {
+            count =1;
+            break;
+        }
+        
+    }
+
+    if (count == 1)
+    {
+        printf("\nstring is not palingdrom");
+    }
+    else
+    {
+        printf("\nstring is palingdrom");
+    }
     
+    
+
     
 
 
