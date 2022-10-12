a150e5f0201d47db78752d05e48d2c9d14d274a9
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2022-10-11 21:45:58 -0700

    Update c129.c

--- c129.c
+++ c129.c
@@ -1,10 +1,10 @@
-
 // ascending order array 
 
 #include<stdio.h>
 main()
 {
     int a[100] = {99,55,96,22,53,87,44,11,66,85,100,1,2};
+    int b[100];
 
     // length of array
     int i,j;
@@ -21,19 +21,30 @@ main()
     int len = i;
 
    
-
+    int k =0;
     for(i=0; i<500; i++)
     {
         for(j=0; j<len; j++)
         {
              if (i == a[j])
              {
-                printf("%d\n",i);
+                //printf(" %d ",i);
+                b[k] = i;
+                printf(" %d ",b[k]);
+                k++;
                 break;
              }
              
         }
     }
+
+        printf("\n");
+
+    for(i=0; i<len; i++)
+    {
+        printf(" %d ",b[i]);
+    }
+
 
 
 
